c4cef63eaa6801204a071945d45c71065b7c9a17
lspss9950101 <lspss9950101@gmail.com>
2020-02-12 00:47:40 +0800

    Fix bugs

--- public/src/graduate/determineValidDestination.js
+++ public/src/graduate/determineValidDestination.js
@@ -11,7 +11,7 @@ function determineValidDestination(req, res, next){
 
 	determineValidDestinationInParallel(req, course)
 	.then((destinations) => {
-		destinations.reduce((acc, val) => if(val.length)acc.concat(val));
+		destinations.reduce((acc, val) => val.length ? acc.concat(val) : acc);
 		req.csca.legalTarget = destinations;
 		next();
 	});
