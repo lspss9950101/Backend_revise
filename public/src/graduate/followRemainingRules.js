e99246b91eababe89d489f6757e2db3aca9bf4f2
lspss9950101 <lspss9950101@gmail.com>
2020-02-12 23:29:59 +0800

    Fix bug.

--- public/src/graduate/followRemainingRules.js
+++ public/src/graduate/followRemainingRules.js
@@ -19,11 +19,13 @@ function handleExcessiveProElective(req){
 			if(one_credit_course_idx == -1)break;
 			let one_credit_course = req.csca.classes.pro_elective.courses[one_credit_course_idx];
 			req.csca.classes.pro_elective.courses.splice(one_credit_course_idx, 1);
+			credit -= 1;
 			req.csca.classes.elective.courses.push(one_credit_course);
 		}else{
 			let three_credit_course_idx = req.csca.classes.pro_elective.courses.findIndex((course) => (course.realCredit == 3));
 			let three_credit_course = req.csca.classes.pro_elective.courses[three_credit_course_idx];
 			req.csca.classes.pro_elective.courses.splice(three_credit_course_idx, 1);
+			credit -= 3;
 			req.csca.classes.elective.courses.push(three_credit_course);
 		}
 	}
