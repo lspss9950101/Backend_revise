4be6b1467db375fecbec7b1cbd3f3e95aba331eb
lspss9950101 <lspss9950101@gmail.com>
2020-02-15 04:10:29 +0800

    Simplify function

--- public/src/common/fetchAndParseData.js
+++ public/src/common/fetchAndParseData.js
@@ -1,11 +1,35 @@
+var query = require('./../../../../../db/msql');
+
 var Course = require('./../graduate/Container/Course.js');
 var CourseRule = require('./../graduate/Container/CourseRule.js');
 
-function parseData(req, res, next){
-	parseDataInParallel(req)
+function fetchAndParseData(req, res, next){
+	req.csca.raw_data = {};
+	fetchDataInParallel(req)
 	.then(() => {
-		next();
+		parseDataInParallel(req)
+		.then(() => (next()));
+	})
+	.catch((err) => {
+		console.error(err);
+		res.redirect('/');
 	});
+}
+
+async function fetchDataInParallel(req){
+	let query_list = req.csca.query_list;
+	let promise_list = [];
+	query_list.forEach((query_target) => {
+		promise_list.push(new Promise((resolve, reject) => {
+			query[query_target.func_name](query_target.syntax, (err, result) => {
+				if(err || result == null)reject({err: err, result: result});
+				req.csca.raw_data[query_target.container_name] = JSON.parse(result);
+				resolve();
+			});
+		}));
+	});
+
+	return await Promise.all(promise_list);
 }
 
 async function parseDataInParallel(req){
@@ -128,4 +152,5 @@ function parseStudentGraduate(req){
 	[req.csca.data.graduate_status] = req.csca.raw_data.student_graduate;
 }
 
-module.exports = parseData;
+
+module.exports = fetchAndParseData;
